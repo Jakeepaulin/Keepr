4caac48b7fcbb403662682623a9bcd9b89277142
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-11-22 15:11:09 -0700

    profilesService updated

--- Keepr.client/src/services/ProfilesService.js
+++ Keepr.client/src/services/ProfilesService.js
@@ -8,6 +8,16 @@ class ProfilesService {
     logger.log(res.data);
     AppState.profile = res.data;
   }
+  async getVaultsForProfiles(id) {
+    const res = await api.get(`api/profiles/${id}/vaults`);
+    console.log("[profileVaults]", res.data);
+    AppState.profileVaults = res.data.map((p) => new Vault(p));
+    console.log(AppState.profileVaults);
+  }
+  async getKeepsForProfiles(id) {
+    const res = await api.get(`api/profiles/${id}/keeps`);
+    AppState.profileKeeps = res.data.map((p) => new Keep(p));
+  }
 }
 
 export const profilesService = new ProfilesService();

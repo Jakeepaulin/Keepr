64e2953ce0847dfcdcb7cdba6126180906c6e881
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-11-23 15:44:41 -0700

    navbar work

--- Keepr.client/src/components/Navbar.vue
+++ Keepr.client/src/components/Navbar.vue
@@ -1,57 +1,60 @@
 <template>
   <nav
-    class="navbar navbar-expand-lg navbar-dark bg-light px-3 d-flex shadow sticky-top"
+    class="navbar navbar-expand-lg navbar-dark bg-light px-3 d-flex shadow sticky-top navShadow"
   >
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center pt-1">
-        <button class="btn btn-outline-dark">Home</button>
+    <div class="row justify-content-between">
+      <div class="d-flex flex-wrap align-content-center">
+        <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+          <div class="d-flex flex-column align-items-center pt-1">
+            <button class="btn btn-outline-dark">Home</button>
+          </div>
+        </router-link>
+        <div class="col-4 dropdown pt-1">
+          <button
+            class="btn btn-secondary dropdown-toggle pt-1"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            v-if="user.isAuthenticated"
+          >
+            Create
+          </button>
+          <ul class="dropdown-menu">
+            <li
+              class="dropdown-item"
+              data-bs-toggle="modal"
+              data-bs-target="#keep-modal"
+            >
+              <button
+                class="btn btn-light"
+                data-bs-toggle="modal"
+                data-bs-target="#keep-modal"
+              >
+                New Keep
+              </button>
+            </li>
+            <li class="dropdown-item">
+              <button
+                class="btn btn-light"
+                data-bs-toggle="modal"
+                data-bs-target="#vault-modal"
+              >
+                New Vault
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
-    </router-link>
 
-    <div class="dropdown pt-1">
-      <button
-        class="btn btn-secondary dropdown-toggle pt-1"
-        type="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-        v-if="user.isAuthenticated"
-      >
-        Create
-      </button>
-      <ul class="dropdown-menu">
-        <li
-          class="dropdown-item"
-          data-bs-toggle="modal"
-          data-bs-target="#keep-modal"
-        >
-          <button
-            class="btn btn-light"
-            data-bs-toggle="modal"
-            data-bs-target="#keep-modal"
-          >
-            New Keep
-          </button>
-        </li>
-        <li class="dropdown-item">
-          <button
-            class="btn btn-light"
-            data-bs-toggle="modal"
-            data-bs-target="#vault-modal"
-          >
-            New Vault
-          </button>
-        </li>
-      </ul>
-    </div>
+      <div class="col-4 pt-1 ps-5">
+        <img src="../assets/img/Keepr logo.png" alt="" height="50" />
+      </div>
 
-    <div class="pt-1 ps-5">
-      <img src="../assets/img/Keepr logo.png" alt="" height="50" />
-    </div>
-
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto"></ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <Login />
+      <div class="col-4 collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto"></ul>
+        <!-- LOGIN COMPONENT HERE -->
+        <Login />
+      </div>
     </div>
   </nav>
 </template>
@@ -80,6 +83,9 @@ export default {
   z-index: 200;
   height: 75px;
 }
+.navShadow {
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
 
 a:hover {
   text-decoration: none;
@@ -93,6 +99,13 @@ a:hover {
   border-bottom: 2px solid var(--bs-success);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
+}
+button:active {
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+}
+.row {
+  flex-wrap: nowrap;
 }
 
 @media screen and (min-width: 768px) {

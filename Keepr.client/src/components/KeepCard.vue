dfad24eb688ee280d1e14df3d04835ba812b8807
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-11-23 15:30:13 -0700

    KeepCard

--- Keepr.client/src/components/KeepCard.vue
+++ Keepr.client/src/components/KeepCard.vue
@@ -1,13 +1,13 @@
 ch
 <template>
-  <div class="rounded">
+  <div class="rounded-4 border-0">
     <div
-      class="card d-flex flex-column justify-content-end elevation-1 shadow selectable rounded"
+      class="card d-flex flex-column justify-content-end elevation-1 shadow selectable rounded-4"
       @click="setActiveKeep()"
       data-bs-toggle="modal"
       :data-bs-target="'#keepDetailsModal' + keep?.Id"
     >
-      <img :src="keep?.img" class="card-img rounded" alt="..." />
+      <img :src="keep?.img" class="cardImg rounded-4" alt="..." />
       <div class="card-img-overlay d-flex flex-column justify-content-end">
         <div class="pb-2 text-light text-shadow d-flex justify-content-between">
           <div>
@@ -74,15 +74,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.keep-card {
-  min-height: 40vh;
-  border: 1rem;
-}
-
 .text-shadow {
   color: aliceblue;
   text-shadow: 1px 1px black, 0px 0px 5px rgb(125, 157, 176);
   font-weight: bold;
   letter-spacing: 0.08rem; /* Second Color  in text-shadow is the blur */
+}
+
+.card {
+  transition: all 0.5s ease;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+  .cardName {
+    text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.25);
+  }
+}
+
+.cardImg {
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+}
+.card:hover {
+  transform: scale(1.01);
+  filter: brightness(90%);
+  transition: all 0.5s ease;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
+    rgba(0, 0, 0, 0.22) 0px 15px 12px;
 }
 </style>
